32a3843ff63dd1684028fa2149e8f51898630025
Preston <powens1@citadel.edu>
2016-10-03 02:52:54 +0000

    Ready for Heroku!

--- app.rb
+++ app.rb
@@ -39,7 +39,10 @@ class HangpersonApp < Sinatra::Base
   # If a guess is invalid, set flash[:message] to "Invalid guess."
   post '/guess' do
     letter = params[:guess].to_s[0]
-    ### YOUR CODE HERE ###
+    @game.guess(letter)
+    if @game.guess(letter) == false
+      flash[:message] = "You have already used that letter."
+    end
     redirect '/show'
   end
   
@@ -50,6 +53,12 @@ class HangpersonApp < Sinatra::Base
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
     ### YOUR CODE HERE ###
+    if @game.check_win_or_lose == :lose
+      redirect '/lose'
+    end
+    if @game.check_win_or_lose == :win
+      redirect '/win'
+    end
     erb :show # You may change/remove this line
   end
   
